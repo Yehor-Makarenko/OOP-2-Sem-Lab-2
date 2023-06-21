1d91cab0b9a044a8772ea975d5677de77025450d
Yehor Makarenko <mak.yehor.knu@gmail.com>
2023-06-21 17:23:48 +0300

    Added Cells Classes

---
+++ js/Classes/CellType.js
@@ -0,0 +1,5 @@
+export default class CellType {
+  constructor(name) {
+    this.name = name;
+  }
+}
--- js/scipt.js
+++ js/scipt.js
@@ -1,5 +1,6 @@
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("screen");
+const context = canvas.getContext("2d");
 const cellWidth = 30;
 const cellHeight = 30;
 
+let col = 0;
