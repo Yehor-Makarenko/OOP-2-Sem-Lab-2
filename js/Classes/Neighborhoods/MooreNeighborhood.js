35d26f6f3cdf54931536bf78b011c457657eb203
Yehor Makarenko <mak.yehor.knu@gmail.com>
2023-06-22 18:13:27 +0300

    Added neighbours classes

---
+++ js/Classes/Neighborhoods/MooreNeighborhood.js
@@ -0,0 +1,9 @@
+export default class NeumannNeighborhood {
+  constructor(range = 1) {
+    this._range = range;
+  }
+
+  isNeighbours(cell1, cell2) {
+    return Math.abs(cell1.x - cell2.x) <= this._range && Math.abs(cell1.y - cell2.y) <= this._range;
+  }
+}
