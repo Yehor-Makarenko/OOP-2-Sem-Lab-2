418a704c5d522e9338bcdcaa4fdb442f67d5ae26
Yehor Makarenko <mak.yehor.knu@gmail.com>
2023-06-23 11:06:27 +0300

    Fixed neighborhood for toroidal coordinates

--- js/Classes/Neighborhoods/MooreNeighborhood.js
+++ js/Classes/Neighborhoods/MooreNeighborhood.js
@@ -1,9 +1,12 @@
 export default class NeumannNeighborhood {
-  constructor(range = 1) {
+  constructor(range = 1, fieldSize) {
     this._range = range;
+    this._fieldSize = fieldSize;    
   }
 
   isNeighbours(cell1, cell2) {
-    return Math.abs(cell1.x - cell2.x) <= this._range && Math.abs(cell1.y - cell2.y) <= this._range;
+    let d1 = this._fieldSize / 2 - Math.abs(Math.abs(cell1.x - cell2.x) - this._fieldSize / 2);
+    let d2 = this._fieldSize / 2 - Math.abs(Math.abs(cell1.y - cell2.y) - this._fieldSize / 2);
+    return d1 <= this._range && d2 <= this._range;
   }
 }
--- js/Classes/Neighborhoods/NeumannNeighborhood.js
+++ js/Classes/Neighborhoods/NeumannNeighborhood.js
@@ -1,9 +1,12 @@
 export default class NeumannNeighborhood {
-  constructor(range = 1) {
+  constructor(range = 1, fieldSize) {
     this._range = range;
+    this._fieldSize = fieldSize;
   }
 
   isNeighbours(cell1, cell2) {
-    return Math.abs(cell1.x - cell2.x) + Math.abs(cell1.y - cell2.y) <= this._range;
+    let d1 = Math.abs(Math.abs(cell1.x - cell2.x) - this._fieldSize / 2);
+    let d2 = Math.abs(Math.abs(cell1.y - cell2.y) - this._fieldSize / 2);
+    return this._fieldSize - d1 - d2
   }
 }
